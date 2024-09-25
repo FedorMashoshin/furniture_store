0e2185bc7e71db21dbc3e9f37ee9c3393ad02778
FedorMAshoshin <fmashoshin@gmail.com>
2024-09-24 22:28:18 -0700

    Moved search field from header to main screen

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -4,7 +4,16 @@ import styles from "./Products.module.css";
 import { API_BASE_URL } from "../../.config";
 import { IProduct } from "../../interfaces/Product";
 import CircularProgress from "@mui/material/CircularProgress";
+import TextField from "@mui/material/TextField";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
 function Products() {
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value);
+        console.log(searchTerm);
+    };
     const [products, setProducts] = useState<IProduct[]>([]);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
@@ -28,15 +37,26 @@ function Products() {
         fetchProducts();
     }, []);
 
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: "#333",
+            },
+        },
+    });
+
     if (isLoading) return <CircularProgress />;
     if (error) return <div>Error: {error}</div>;
 
     return (
-        <main className={styles.productsList}>
-            {products.map((item) => (
-                <Product key={item._id} {...item} />
-            ))}
-        </main>
+        <ThemeProvider theme={theme}>
+            <TextField id="outlined-basic" label="Search" variant="outlined" value={searchTerm} onChange={handleSearch} margin="normal" sx={{ width: "30%" }} />
+            <main className={styles.productsList}>
+                {products.map((item) => (
+                    <Product key={item._id} {...item} />
+                ))}
+            </main>
+        </ThemeProvider>
     );
 }
 
